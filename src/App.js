8cae390975076eaea9daaac7bb00e7ad45281fd7
Gonuguntla-Chandana <gonuguntlachandana093@gmail.com>
2025-03-22 20:38:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react'
+ import Navbar from './Components/Navbar'
+ import Home from './Pages/Home'
+ import Login from './Pages/Login'
+ import Register from './Pages/Register' 
+ import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ const App = () => {
+   return (
+     <Router>
+      <Navbar />
+      <Routes>
+         <Route path="/" element={<Home/>} />
+         <Route path="/Login" element={< Login />} />
+         <Route path="/Register" element={< Register />} />
+     </Routes>
+     </Router> 
+ }
+ 
+ export default App
